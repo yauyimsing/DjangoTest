28385e9b0fe86074041bbf1a3649dc19f74a4a79
yauyimsing <yauyimsing@gmail.com>
2019-03-28 14:49:58 +0800

    configurate views and templates

--- DjangoProject/booktest/views.py
+++ DjangoProject/booktest/views.py
@@ -1,3 +1,12 @@
+#coding=utf-8
 from django.shortcuts import render
-
+from models import *
 # Create your views here.
+def index(request):
+    list = BookInfo.books1.all()
+    print('hello kugou')
+    for l in list:
+        print(l.btitle)
+        print('hh......')
+    context={'list':list}
+    return render(request, 'booktest/index.html', context)
